7d32d72411d8729a73117eb7553d6ecc71cfe281
Ruslan Shkorbatov <r.shkorbatov@yandex.ru>
2018-08-03 14:56:20 +0300

    Оформление переноса
    Добавлен combobox (список реестров).
    Добавлена кнопка переноса.
    Добавлено сообщение окончания операции.

--- Yamed.Oms/ReestrChooseControl.xaml.cs
+++ Yamed.Oms/ReestrChooseControl.xaml.cs
@@ -16,6 +16,8 @@ using DevExpress.Mvvm.Native;
 using Yamed.Control;
 using Yamed.Core;
 using Yamed.Server;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Yamed.Oms
 {
@@ -46,6 +48,7 @@ namespace Yamed.Oms
 
             var upd = Reader2List.CustomUpdateCommand("D3_ZSL_OMS", updObj, "ID");
             Reader2List.CustomExecuteQuery(upd, SprClass.LocalConnectionString);
+            MessageBox.Show("Перенос записей выполнен успешно.", "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
         }
 
 
@@ -65,5 +68,27 @@ namespace Yamed.Oms
             return ids;
         }
 
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            using (SqlConnection sc = new SqlConnection(SprClass.LocalConnectionString))
+            {
+                sc.Open();
+                using (SqlDataAdapter sda = new SqlDataAdapter("select s.id as ID, case   when s.month = 1 then concat('Реестр счета (', s.id, ') Январь ', s.year,' г.')   when s.month = 2 then concat('Реестр счета (', s.id, ') Февраль ', s.year,' г.')   when s.month = 3 then concat('Реестр счета (', s.id, ') Март ', s.year,' г.')   when s.month = 4 then concat('Реестр счета (', s.id, ') Апрель ', s.year,' г.')   when s.month = 5 then concat('Реестр счета (', s.id, ') Май ', s.year,' г.')   when s.month = 6 then concat('Реестр счета (', s.id, ') Июнь ', s.year,' г.')   when s.month = 7 then concat('Реестр счета (', s.id, ') Июль ', s.year,' г.')   when s.month = 8 then concat('Реестр счета (', s.id, ') Август ', s.year,' г.')   when s.month = 9 then concat('Реестр счета (', s.id, ') Сентябрь ', s.year,' г.')   when s.month = 10 then concat('Реестр счета (', s.id, ') Октябрь ', s.year,' г.')   when s.month = 11 then concat('Реестр счета (', s.id, ') Ноябрь ', s.year,' г.')   when s.month = 12 then concat('Реестр счета (', s.id, ') Декабрь ', s.year,' г.') end nameSchet from d3_schet_oms s", sc))
+                {
+                    using (DataTable dt = new DataTable())
+                    {
+                        sda.Fill(dt);
+                        comboboxSchets.ItemsSource = dt;
+                    }
+                }
+                sc.Close();
+                comboboxSchets.SelectedIndex = -1;
+            }
+        }
+
+        private void comboboxSchets_SelectedIndexChanged(object sender, RoutedEventArgs e)
+        {
+            ButtonEdit.EditValue = ((DataRowView)comboboxSchets.SelectedItem)[comboboxSchets.ValueMember].ToString();
+        }
     }
 }
