314555ff9b7a95d24814e209db97e7a05bd0cab1
Ruslan Shkorbatov <r.shkorbatov@yandex.ru>
2018-08-17 13:00:44 +0300

    added copying in policlinic

--- Yamed.Oms/ReestrChooseControl.xaml.cs
+++ Yamed.Oms/ReestrChooseControl.xaml.cs
@@ -38,6 +38,22 @@ namespace Yamed.Oms
 
         private void buttonStart_Click(object sender, RoutedEventArgs e)
         {
+            int warn = 0;
+            using (SqlConnection sconn = new SqlConnection(SprClass.LocalConnectionString))
+            {
+                sconn.Open();
+                using (SqlCommand scomm = new SqlCommand("select Parametr from Settings where Name = 'MedicalOrganization' and Parametr = '460024'", sconn))
+                {
+                    using (SqlDataReader sreader = scomm.ExecuteReader())
+                    {
+                        while (sreader.Read())
+                        {
+                            warn = Convert.ToInt32(sreader["Parametr"]);
+                        }
+                    }
+                }
+                sconn.Close();
+            }
             _id = (int)cbSchets.EditValue;
             int selectCbOne = cbOperation.SelectedIndex;
             if (selectCbOne == 1)
@@ -57,21 +73,12 @@ namespace Yamed.Oms
                 Reader2List.CustomExecuteQuery(upd, SprClass.LocalConnectionString);
                 MessageBox.Show("Перенос записей выполнен успешно.", "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
             }
-            if (selectCbOne == 2)
+            if (selectCbOne == 0 & warn != 460028)
             {
                 var ids = GetIds(DxHelper.LoadedRows.Select(x => ObjHelper.GetAnonymousValue(x, "ID")).OfType<int>().ToArray());
-                var insObjZsl = (IList)Reader2List.CustomAnonymousSelect($"select * from d3_zsl_oms where id in ({ids})", SprClass.LocalConnectionString);
-                var insObjSl = (IList)Reader2List.CustomAnonymousSelect($"select * from d3_sl_oms where d3_zslid in ({ids})", SprClass.LocalConnectionString);
-                var insObjPacient = (IList)Reader2List.CustomAnonymousSelect($"select * from d3_pacient_oms where id in (select d3_pid from d3_zsl_oms where id in ({ids}))", SprClass.LocalConnectionString);
-                var insObjUsl = (IList)Reader2List.CustomAnonymousSelect($"select * from d3_usl_oms where d3_zslid in (select id from d3_zsl_oms where id in ({ids}))", SprClass.LocalConnectionString);
-                foreach (var row in insObjPacient)
-                {
-                    var r = row;
-                    ObjHelper.SetAnonymousValue(ref r, _id, "D3_SCID");
-                    ObjHelper.SetAnonymousValue(ref r, Guid.NewGuid(), "ID_PAC");
-                }
-                var ins = Reader2List.CustomInsertCommand("d3_pacient_oms", insObjPacient, "ID", SprClass.LocalConnectionString);
-                Reader2List.CustomExecuteQuery(ins.ToString(), SprClass.LocalConnectionString);
+                var copyQuery = "IF OBJECT_ID(N'tempdb..#t_pac', N'U') IS NOT NULL DROP TABLE #t_pac;  IF OBJECT_ID(N'tempdb..#t_zsl', N'U') IS NOT NULL DROP TABLE #t_zsl;  IF OBJECT_ID(N'tempdb..#t_sl', N'U') IS NOT NULL DROP TABLE #t_sl;  IF OBJECT_ID(N'tempdb..#t_usl', N'U') IS NOT NULL DROP TABLE #t_usl;  alter table D3_PACIENT_OMS add rn int ;  alter table D3_ZSL_OMS add rn int,rnp int;  alter table D3_SL_OMS add rn int,rnz int;  alter table D3_uSL_OMS add rn int,rns int;     declare @cols varchar(max),@cols1 varchar(max), @query varchar(max), @id varchar(max),@cols2 varchar(max),@cols3 varchar(max); set @id='" + ids + "' SELECT  @cols = STUFF    (         (             SELECT  '], [' + name             FROM sys.columns             where object_id = (                 select  object_id from sys.objects                 where name = 'D3_PACIENT_OMS'             )             and name not in (SELECT  Name  FROM    sys.computed_columns where OBJECT_NAME(object_id)='D3_PACIENT_OMS') 			and name!='rn'             FOR XML PATH('')         ), 1, 2, ''     ) + ']';  	SELECT  @cols1 = STUFF    (         (             SELECT  '], [' + name             FROM sys.columns             where object_id = (                 select  object_id from sys.objects                 where name = 'D3_ZSL_OMS'             )             and name not in (SELECT  Name  FROM    sys.computed_columns where OBJECT_NAME(object_id)='D3_ZSL_OMS') 			and name!='rn' and name!='rnp'             FOR XML PATH('')         ), 1, 2, ''     ) + ']';  	SELECT  @cols2 = STUFF    (         (             SELECT  '], [' + name             FROM sys.columns             where object_id = (                 select  object_id from sys.objects                 where name = 'D3_SL_OMS'             )             and name not in (SELECT  Name  FROM    sys.computed_columns where OBJECT_NAME(object_id)='D3_SL_OMS') 			and name!='rn' and name!='rnz'             FOR XML PATH('')         ), 1, 2, ''     ) + ']';  	SELECT  @cols3 = STUFF    (         (             SELECT  '], [' + name             FROM sys.columns             where object_id = (                 select  object_id from sys.objects                 where name = 'D3_USL_OMS'             )             and name not in (SELECT  Name  FROM    sys.computed_columns where OBJECT_NAME(object_id)='D3_USL_OMS') 			and name!='rn' and name!='rns'             FOR XML PATH('')         ), 1, 2, ''     ) + ']';  	declare @cols10 varchar(max); 	set @cols10=replace(@cols,'[ID],',''); 	declare @cols11 varchar(max); 	set @cols11=replace(@cols1,'[ID],',''); 	declare @cols12 varchar(max); 	set @cols12=replace(@cols2,'[ID],',''); 	declare @cols13 varchar(max); 	set @cols13=replace(@cols3,'[ID],','');  SELECT @query = 'select ROW_NUMBER ( ) OVER (order by id ) as RN,' + @cols + 'into #t_pac from D3_PACIENT_OMS where  id in (select d3_pid from d3_zsl_oms where id in ('+@id+')) update #t_pac set ID_PAC=newid(),D3_SCID=" + _id + "  select ROW_NUMBER ( ) OVER (order by id ) as RN,0 as rnp,' + @cols1 + 'into #t_zsl from D3_ZSL_OMS where  id in ('+@id+') update #t_zsl set #t_zsl.ZSL_ID=NEWID() 	  	  update #t_zsl  set #t_zsl.rnp=#t_pac.RN from #t_pac where #t_zsl.D3_PID=#t_pac.ID 	  update #t_zsl set #t_zsl.D3_PGID=#t_pac.ID_PAC,D3_SCID=" + _id + " from #t_pac where #t_zsl.D3_PID=#t_pac.ID   select ROW_NUMBER ( ) OVER (order by id ) as RN,0 as rnz,' + @cols2 + 'into #t_sl from D3_SL_OMS where D3_ZSLID in (select id from #t_zsl) update #t_sl  set #t_sl.rnz=#t_zsl.RN from #t_zsl where #t_sl.D3_zslID=#t_zsl.ID 	  update #t_sl set #t_sl.D3_ZSLGID=#t_zsl.ZSL_ID from #t_zsl where #t_sl.D3_ZSLID=#t_zsl.ID 	  update #t_sl set #t_sl.SL_ID=NEWID()   select ROW_NUMBER ( ) OVER (order by id ) as RN,0 as rns,' + @cols3 + 'into #t_usl from D3_USL_OMS where D3_ZSLID in (select id from #t_zsl)  update #t_usl  set #t_usl.rns=#t_sl.RN from #t_sl where #t_usl.D3_slID=#t_sl.ID 	  update #t_usl set #t_usl.D3_SLGID=#t_sl.SL_ID from  #t_sl where #t_usl.D3_SLID=#t_sl.ID    insert into D3_PACIENT_OMS (rn,'+@cols10+') select rn,'+@cols10+' from #t_pac  insert into D3_ZSL_OMS (rn,rnp,'+@cols11+') select rn,rnp,'+@cols11+' from #t_zsl update D3_ZSL_OMS set D3_PID= D3_PACIENT_OMS.id from D3_PACIENT_OMS where D3_ZSL_OMS.rnp=D3_PACIENT_OMS.rn  insert into D3_SL_OMS (rn,rnz,'+@cols12+') select rn,rnz,'+@cols12+' from #t_sl update D3_SL_OMS set D3_ZSLID= d3_zsl_oms.ID from D3_ZSL_OMS where D3_SL_OMS.rnz=D3_ZSL_OMS.rn  insert into D3_USL_OMS (rn,rns,'+@cols13+') select rn,rns,'+@cols13+' from #t_usl update D3_USL_OMS set D3_ZSLID= d3_sl_oms.D3_ZSLID from D3_SL_OMS where D3_uSL_OMS.rns=D3_SL_OMS.rn 	  update D3_USL_OMS set D3_SLID= d3_sl_oms.ID from D3_SL_OMS where D3_uSL_OMS.rns=D3_SL_OMS.rn 	  update D3_USL_OMS set D3_SLGID= d3_sl_oms.SL_ID from D3_SL_OMS where D3_uSL_OMS.rns=D3_SL_OMS.rn  '; EXEC (@query);  alter table D3_PACIENT_OMS drop column rn; alter table D3_zsl_OMS drop column rn; alter table D3_zsl_OMS drop column rnp; alter table D3_sl_OMS drop column rn; alter table D3_sl_OMS drop column rnz; alter table D3_usl_OMS drop column rn; alter table D3_usl_OMS drop column rns;";
+                Reader2List.CustomExecuteQuery(copyQuery, SprClass.LocalConnectionString);
+                MessageBox.Show("Копирование записей выполнено успешно.", "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
             }
         }
 
